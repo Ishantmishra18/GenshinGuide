ebcf1a40bcafeb8fb2cb0c199b59dff791f9cd99
unknown <shotlast901@gmail.com>
2024-04-20 03:48:05 +0530

    navbar temp completed char selection feature is completed

---
+++ data.js
@@ -0,0 +1,6 @@
+function data(){
+let charinfo={
+    bennet:["sword","this set is very good for the bennett but I personally love the great wolf sword"],  
+    xiangling:["bow"]
+}
+}
--- script.js
+++ script.js
@@ -1,62 +1,96 @@
+
+
 let cards=document.querySelectorAll(".card")
-cards.forEach(function(card){
-  card.addEventListener("mouseover",function(){
-      let char=card.childNodes[1].childNodes[1]
-      let bg=card.childNodes[1]
-      bg.style.backgroundSize="320px"
-      card.style.transform="scale(1.06)"
-      char.style.transform="scale(1.12) translateY(-10px)"
+function Hover(){
+    cards=document.querySelectorAll(".card")
+                cards.forEach(function(cardo , index){
+                    const animationDelay = 0.2;
+                    cardo.style.animationDelay = `${index * animationDelay}s`;
+                    cardo.addEventListener("mouseover",function(){
+                        let char=cardo.childNodes[1].childNodes[1]
+                        let bg=cardo.childNodes[1]
+                        bg.style.backgroundSize="320px"
+                        cardo.style.transform="scale(1.06)"
+                        char.style.transform="scale(1.12) translateY(-10px)"
+                  
+                    })
+                    cardo.addEventListener("mouseleave",function(){
+                        let char=cardo.childNodes[1].childNodes[1]
+                        let bg=cardo.childNodes[1]
+                        bg.style.backgroundSize="400px"
+                        cardo.style.transform="scale(0.75)"
+                        char.style.transform="scale(1)"
+                        
+                    })
+                    cardo.addEventListener("click",function(){
+                        cardshow.innerHTML=` <div class="charimgi">
+                        <img src="char/noob2.png" alt="">`
+                    })
+                   
+                  })}
+    
 
-  })
-  card.addEventListener("mouseleave",function(){
-      let char=card.childNodes[1].childNodes[1]
-      let bg=card.childNodes[1]
-      bg.style.backgroundSize="400px"
-      card.style.transform="scale(1)"
-      char.style.transform="scale(1)"
-      
-  })
-})
   
-
-
 let elements = ["pyro", "cryo", "hydro", "dendro", "geo", "anemo", "electro"];
 let characters = {
-    pyro: ["bennett", "alerchino", "xiangling"],
-    cryo: ["ayaka", "ganyu", "wriothesley"],
-    hydro: ["neuvillette", "childe", "yelan", "kokomi", "furina", "ayato"],
-    anemo: ["venti", "xiao", "kazuha"],
-    electro: ["raiden shogun", "yae miko"],
-    dendro: ["nahida", "Alhaitam", "tainari", "baizhu"],
-    geo: ["arataki itto", "navia"]
+    pyro: {name:["bennet", "yoimiya", "xiangling"],
+            weapon:["sword","bow","polearm"],
+        rarity:["4star","5star","4star"]},
+
+    cryo: {name:["shenhe", "ganyu", "ayaka"],
+            weapon:["sword","bow","polearm"]},
+    hydro: {name:["neuvillette", "childe", "furina"],
+            weapon:["sword","bow","polearm"]},
+    anemo:{name:["venti", "xiao", "kazuha"],
+            weapon:["sword","bow","polearm"]},
+    electro: {name:["raiden shogun" ,"venti" ,"yelan" ,"yoimiya","xiangling","shenhe","furina","childe","kokomi" ,"tighnari","xiao","ganyu","arataki itto","alhaitham",],
+            weapon:["sword","bow","polearm"]},
+    dendro: {name:["nahida", "baizhu", "alhaitam" , "tighnari"],
+            weapon:["sword","bow","polearm"]},
+    geo: {name:["arataki itto", "zhongli"],
+            weapon:["sword","bow","polearm"]},
 };
 
+let cardshow = document.querySelector(".cardshowcase");
 let elementsbtn = document.querySelectorAll(".elem");
 elementsbtn.forEach(function(elem) {
     elem.addEventListener("click", function() {
-        let cardshow = document.querySelector(".cardshowcase");
+        
+        
         cardshow.innerHTML = "";
         let elementClicked = elem.getAttribute('elem');
         if (characters.hasOwnProperty(elementClicked)) {
-            characters[elementClicked].forEach(character => {
+            characters[elementClicked].name.forEach(character => {
                 let characterImageSrc = `char/${character}.png`;
                 console.log(character);
-                cardshow.innerHTML += `<div class="card">
+                
+                cardshow.innerHTML += `<div class="card ${character}">
                     <div class="bg">
                         <img src="${characterImageSrc}" alt="${character}">
                         <span class="text">${character}</span>
                         <div class="bottom"></div>
                     </div>
                 </div>`;
-               
+                Hover()
+                
             });
         }
-        cards=document.querySelectorAll(".card")
+        
     });
 });
+Hover()
 
-
-              
+let elements2=document.querySelectorAll(".celem")
+elements2.forEach(function(celem){
+    celem.addEventListener("click",function(){
+        window.location.href = "index.html";
+    })
+})
+function handleCardClick() {
+    const card = document.querySelector('.click');
+    card.classList.add('clicked'); // Add a class to the card to trigger the transition
+}
+             
             
                    
               
